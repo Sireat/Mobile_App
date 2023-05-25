7e1b4a96805406e3298e3e27020f041bc1aedfee
Sireat <sireatg@gmail.com>
2023-05-25 15:09:58 +0300

    gpt3  app

---
+++ chatGPT/gpt3/lib/main.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
+import 'package:openai/gpt_3.dart';
+import 'package:openai/home.dart';
+
+void main() async {
+  GestureBinding.instance.resamplingEnabled = true;
+  WidgetsFlutterBinding.ensureInitialized();
+  OpenAI openAI = new OpenAI(apiKey: "YOUR_KEY_HERE");
+  runApp(MyApp(openAI));
+}
+
+class MyApp extends StatelessWidget {
+  final OpenAI openAI;
+  MyApp(this.openAI);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'OpenAI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Home(openAI),
+    );
+  }
+}
